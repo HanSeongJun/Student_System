a6451f9d3cc954f7c046deb54c07ba95f1322279
HanSeongJun <gkstjdwns2@naver.com>
2023-04-21 18:52:58 +0900

    first commit

---
+++ src/main/java/backend/studentsystem/service/StudentService.java
@@ -0,0 +1,11 @@
+package backend.studentsystem.service;
+
+import backend.studentsystem.model.Student;
+
+import java.util.List;
+
+public interface StudentService {
+
+    public Student saveStudent(Student student);
+    public List<Student> getAllStudents();
+}
